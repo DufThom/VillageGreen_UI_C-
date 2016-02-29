e3b8ede9c8952a34fe2d3f5e7c5d346da5e3484c
ThomasD <dufaux.thom@gmail.com>
2016-02-29 16:59:40 +0100

    Ouverture Projet Interface utilisateur pour le Fil Rouge Village Green, création DAL avec Requête "Liste"

---
+++ DAL/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Client
+    {
+        public int Id { get; set;  } //PK, int, Not Null
+
+        public string NomClient { get; set;  } //varchar100, Not Null
+
+        public bool ClientPro { get; set;  } //Bit, Not Null
+
+        public string AdressFact { get; set; } //varchar200, Not Null
+
+        public string AdressLiv { get; set; } //varchar200,   Null
+
+        public int IdComm { get; set; } //FK, int, Not Null
+
+
+    }
+}
---
+++ DAL/ClientDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    class ClientDAO
+    {
+        // +++++++++++++++++++++ Partie 1 : Interrogation (Saisie & Affichage) ++++++++++++++++++++++++++++++++++++++++ 
+
+
+
+        // +++++++++++++++++++++ Partie 2 : Gestion d'une Table (Ajout, Affichage, Modification, Suppression) ++++++++++
+
+
+        // +++++++++++++++++++++ Affichage complet ou Unitaire +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public List<Client> List()
+        {
+            List<Client> Liste = new List<Client>();
+
+            SqlConnection Connex = new SqlConnection("server=.; database=Village_Green; integrated security= true");
+            Connex.Open();
+
+            SqlCommand Reqt = new SqlCommand("select * from stg07.Client", Connex);
+
+            SqlDataReader Result = Reqt.ExecuteReader();
+
+            while (Result.Read())
+            {
+                Client Cli = new Client();
+                Cli.Id = Convert.ToInt32(Result["IdClient"]);
+                Cli.NomClient = Convert.ToString(Result["NomClient"]);
+                Cli.ClientPro = Convert.ToBoolean(Result["ClientProfessionnel"]);
+                Cli.AdressFact = Convert.ToString(Result["AdressFacturClient"]);
+                Cli.AdressLiv = Convert.ToString(Result["AdressLivraisonClient"]);
+                Cli.IdComm = Convert.ToInt32(Result["IdCommercial"]);
+
+                Liste.Add(Cli);
+            }
+            Connex.Close();
+            return Liste;
+        }
+
+        //##### ici, faire le Find #############################
+
+    }
+}
