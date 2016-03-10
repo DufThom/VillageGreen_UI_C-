23b9daa7860266fe28f38599a2a28acbdcd045a1
ThomasD <dufaux.thom@gmail.com>
2016-03-10 16:23:20 +0100

    Ajout Console Contrôle de Mail, avec Test Unitaires

---
+++ Mail_Console/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Mail_Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Contrôle que l'adresse Mail saisie est valide, ex. de mail valide : "x x @ x x . x x " 
+            // soit 2 caractères mini avant l'@, 2 caractères mini entre l'@ et le ".", et 2 caractères mini après le "."
+            // ^ : début de la ligne
+            // [a - zA - Z]+ : une lettre (au moins une)
+            // [a-zA-Z0-9\\._-]* : une lettre ou un chiffre ou un "." ou un "_" ou un "-" autant de fois que l'on veut (0 à n)
+            // [a-zA-Z0-9]+ : une lettre ou un chiffre(au moins 1)
+            // @ : le symbole @ (obligatoire)
+            // [a-zA-Z]+ : une lettre(au moins une)
+            // [a-zA-Z0-9\\._-]* : une lettre ou un chiffre ou un "." ou un "_" ou un "-" autant de fois que l'on veut (0 à n)
+            // [a-zA-Z0-9]+ : une lettre ou un chiffre(au moins 1)
+            // \\. : un "." (obligatoire)
+            // [a-zA-Z]{2,4} : de 2 à 4 lettres
+            // $ : fin de ligne
+
+            Console.WriteLine("Entrez une adresse Mail : \n");
+            string Mail = Convert.ToString(Console.ReadLine()); // Adresse complète
+            string m = Cont_Mail(Mail);
+            Console.WriteLine(m);
+
+            Console.ReadLine();
+
+        }
+
+        public static string Cont_Mail(string Mail)
+        {
+            string[] Mail1; // mot à Gauche de l'@
+            string[] Mail2; // mot après l'@ 
+            char Arob = '@';
+            char Point = '.';
+            string Mess = "";
+
+            if (Mail.IndexOf(Arob) == -1)
+                {
+                Mess = "il manque l'@";
+                }
+                else
+                {
+                    Mail1 = Mail.Split(Arob);
+                    if (Mail1[1].IndexOf(Point) == -1)
+                    {
+                        Mess = ("Manque le Point ");
+                    }
+                    else
+                    {
+                        Mail2 = Mail1[1].Split(Point);
+                        //Console.WriteLine("ok");
+                        Regex testMail1 = new Regex("^[a-zA-Z0-9]{2,}(([_.-]?)[a-zA-Z0-9])*$");
+                        Regex testMail2 = new Regex("^[a-zA-Z0-9]{2,}$");
+
+                        if (!testMail1.IsMatch(Mail1[0]))
+                        {
+                            Mess = ("vérifiez le mail avant l'@ ");
+                        }
+                        else
+                        {
+                            if (!testMail1.IsMatch(Mail2[0]))
+                            {
+                                Mess = ("vérifiez entre l'@ et le Point ");
+                            }
+                            else
+                            {
+                                if (!testMail2.IsMatch(Mail2[1]))
+                                {
+                                    Mess = ("vérifiez après le Point");
+                                }
+                                else
+                                {
+                                    Mess = "OK";
+                                    //Console.WriteLine(Mess);
+                                    
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            return Mess;
+        }
+
+    }
+    }
+
---
+++ UnitTest_Mail/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mail_Console; 
+
+namespace UnitTest_Mail
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string Mail = "test@test.fr"; // mail bon
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "OK");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string Mail = "test.test.fr"; //Manque @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreNotEqual(Result, "OK");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string Mail = "test.@test.fr"; // Point devant @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreNotEqual(Result, "OK");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string Mail = "t@test.fr"; // 1 seul caractères devant @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreNotEqual(Result, "OK");
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string Mail = "test@testfr"; // Manque Point après @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreNotEqual(Result, "OK");
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string Mail = "test.fr"; // manque @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "il manque l'@");
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            string Mail = "test@testfr"; // manque Point
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "Manque le Point ");
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            string Mail = "t@test.fr"; // 1 seul caract, avant @
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "vérifiez le mail avant l'@ ");
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            string Mail = "test@t.fr"; // 1 seul caract, entre @ et Point
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "vérifiez entre l'@ et le Point ");
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            string Mail = "test@test.f"; // 1 seul caract, après Point
+            string Result = Program.Cont_Mail(Mail);
+            Assert.AreEqual(Result, "vérifiez après le Point");
+        }
+    }
+}
